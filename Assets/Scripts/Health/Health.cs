dce702d3256d0f42bbdabd73e6012194836ea198
PacifistBluey <jldechant999@gmail.com>
2022-12-21 16:44:26 -0500

    Tutorial 10
    Adding finished Unity tutorial 10

---
+++ Assets/Scripts/Health/Health.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+    [Header("Health")]
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+    private Animator anim;
+    private bool dead;
+
+    [Header("iframes")]
+    [SerializeField] private float iframesDuration;
+    [SerializeField] private int numberOfflashes;
+    private SpriteRenderer spriteRend;
+
+    [Header("Components")]
+    [SerializeField] private Behaviour[] components;
+    private bool invurnerable;
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+
+    public void TakeDamage(float _damage)
+    {
+        if (invurnerable) return;
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth > 0)
+        {
+            //player hurt
+            anim.SetTrigger("hurt");
+            //iframes
+            StartCoroutine(Invunerability());
+        }
+        else
+        {
+            //player dead
+            if (!dead)
+            {
+                anim.SetTrigger("die");
+
+                //Deactivate all attached component classes
+                foreach (Behaviour component in components)
+                {
+                    component.enabled = false;
+                }
+
+                dead = true;
+            }
+        }
+    }
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+
+    private IEnumerator Invunerability()
+    {
+        invurnerable = true;
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        //invunerability duration
+        for (int i = 0; i < numberOfflashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iframesDuration / (numberOfflashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iframesDuration / (numberOfflashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(10, 11, false);
+        invurnerable = false;
+    }
+
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
+}
