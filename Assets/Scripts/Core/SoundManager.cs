6eabb3846ccbd9ea9d362c287bc4efb59f3fa931
PacifistBluey <jldechant999@gmail.com>
2022-12-22 12:47:07 -0500

    Finished
    Added BGM and new test scene

--- Assets/Scripts/Core/SoundManager.cs
+++ Assets/Scripts/Core/SoundManager.cs
@@ -7,19 +7,21 @@ public class SoundManager : MonoBehaviour
 
     private void Awake()
     {
-        instance = this;
         source = GetComponent<AudioSource>();
+
+        //Keep this object even when we go to a new scene
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        //Destroy duplicate gameobjects
+        else if (instance != null && instance != this)
+            Destroy(gameObject);
     }
 
     public void PlaySound(AudioClip _sound)
     {
         source.PlayOneShot(_sound);
     }
-
-    /*
-    [Header("SFX")]
-    [SerializeField] private AudioClip jumpSound;
-
-    SoundManager.instance.PlaySound(jumpSound);
-    */
 }
