8c7a43e20b1659c1160ffed9ad0f84006d56a353
PacifistBluey <jldechant999@gmail.com>
2022-12-23 18:32:33 -0500

    SelectionArrow Functions
    Added selection arrow functions

--- Assets/Scripts/UI/SelectionArrow.cs
+++ Assets/Scripts/UI/SelectionArrow.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SelectionArrow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private RectTransform[] options;
+    [SerializeField] private AudioClip changeSound; //Option changed
+    [SerializeField] private AudioClip interactSound; //Option selected
+    private RectTransform rect;
+    private int currentPosition;
+
+    private void Awake()
     {
-        
+        rect = GetComponent<RectTransform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            ChangePosition(-1);
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            ChangePosition(1);
+    }
+
+    private void ChangePosition(int _change)
+    {
+        currentPosition += _change;
+
+        if (_change != 0)
+            SoundManager.instance.PlaySound(changeSound);
+
+        if (currentPosition < 0)
+            currentPosition = options.Length - 1;
+        else if (currentPosition > options.Length - 1)
+            currentPosition = 0;
+
+        //Assign the Y position of the current option to the arrow(up/down)
+        rect.position = new Vector3(rect.position.x, options[currentPosition].position.y, 0);
     }
 }
