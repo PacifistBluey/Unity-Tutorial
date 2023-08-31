a128f4b7cb6e109e3e0c1c3ed460f3bb9c4644df
PacifistBluey <jldechant999@gmail.com>
2023-08-31 13:29:35 -0400

    Character Sprites

--- Assets/Scripts/DialougeSystem/DialogueLine.cs
+++ Assets/Scripts/DialougeSystem/DialogueLine.cs
@@ -12,11 +12,17 @@ namespace DialogueSystem
         [SerializeField] private string input;
         [SerializeField] private float delay;
         [SerializeField] private AudioClip sound;
+        [SerializeField] private Sprite characterSprite;
+        [SerializeField] private Image imageHolder;
 
         private void Awake()
         {
             textHolder = GetComponent<Text>();
+            textHolder.text = "";
+
             StartCoroutine(WriteText(input, textHolder, delay, sound));
+            imageHolder.sprite = characterSprite;
+            imageHolder.preserveAspect = true;
         }
     }
 }
